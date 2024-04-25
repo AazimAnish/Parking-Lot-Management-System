967e4556bd0925f767dd753d7691a4aaba092f2b
AazimAnish <aazimanish1@gmail.com>
2024-04-25 22:03:48 +0530

    vehicle parked

--- ParkingLot.java
+++ ParkingLot.java
@@ -20,4 +20,26 @@ public class ParkingLot {
             }
         }
     }
+
+    String parkVehicle(String type, String regNo, String color) {
+        Vehicle vehicle = new Vehicle(type, regNo, color);
+
+        for(int i=0; i<slots.size();i++){
+            for(int j=0;j<slots.get(i).size(); j++){
+                Slot slot = slots.get(i).get(j);
+                if(slot.type == type && slot.vehicle == null) {
+                    slot.vehicle=vehicle;
+                    slot.ticketId=generateTicketId(i+1, j+1);
+                    return slot.ticketId;
+                }
+            }
+        } 
+        System.out.println("NO slot available for given type");
+        return null;
+        
+    }
+
+    private String generateTicketId(int flr, int slno){
+        return parkingLotId + "_" + flr + "_" + slno;
+    }
 }
